89316f0fd269c61a3c7da84736c9bee0fc8dc1c3
Josh Cesana <joshuacesana@gmail.com>
2018-07-08 20:01:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,9 +43,9 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
-    FROM projects
-    LEFT JOIN pledges 
-    ON projects.id = pledges.project_id
+    FROM pledges
+    LEFT JOIN projects 
+    ON pledges.project_id = projects.id 
     WHERE projects.category = 'music';"
 end
 
